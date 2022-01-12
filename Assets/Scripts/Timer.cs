94165150ae0b71004100b93adecc83e4b7e075e7
Younes4260 <70180315+Younes4260@users.noreply.github.com>
2022-01-12 18:01:22 -0500

    Finalisation Timer

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
-    public float LeTimer = 60;
+    public float LeTimer;
     public Text TimerText;
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,12 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (LeTimer < 0)
+        {
+            LeTimer = 0;
+            // Inserer le changement de scene ici
+            // SceneManager.LoadScene("");
+        }
         LeTimer -= Time.deltaTime;
         TimerText.text = LeTimer.ToString("f0");
     }
