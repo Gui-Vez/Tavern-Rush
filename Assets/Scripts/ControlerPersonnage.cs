137fe328b2b9a423e474a14f835a52e919159cfd
Younes4260 <70180315+Younes4260@users.noreply.github.com>
2022-01-12 12:04:35 -0500

    Fix script mineur

--- Assets/Scripts/ControlerPersonnage.cs
+++ Assets/Scripts/ControlerPersonnage.cs
@@ -11,8 +11,7 @@ public class ControlerPersonnage : MonoBehaviour
 
 
     /* Détection des touches et modification de la vitesse de déplacement;
-       "a" et "d" pour avancer et reculer, "w" pour sauter
-    */
+       "a" et "d" pour aller a gauche et a droite, "w" et "s" pour en haut et en bas */
     void Update()
     {
         vitesseY = Mathf.Round(Input.GetAxis("Vertical")) * Vitesse;
@@ -30,7 +29,7 @@ public class ControlerPersonnage : MonoBehaviour
         if (collision.gameObject.tag == "Mouche")
         {
         // Insérer ici tout ce qui est nécessaire après le contact de la mouche
-            Debug.Log("hit");
+        //    Debug.Log("hit");
         }
     }
    
